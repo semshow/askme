6a90d84f03a58b5abc545d43e4ed0bd95dbbef73
alexdermenji <semshow@mail.ru>
2019-02-27 13:33:17 +0000

    views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,36 @@
 class UsersController < ApplicationController
+  def index
+    # Создаём массив из двух болванок пользователей. Вызываем метод # User.new, который создает модель, не записывая её в базу.
+    # У каждого юзера мы прописали id, чтобы сымитировать реальную
+    # ситуацию – иначе не будет работать хелпер путей
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/' \
+        '71269686e0f757ddb4f73614f43ae445?s=100'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Hi Alex"
+    @user = User.new(
+      name: 'Vadim',
+      username: 'installero',
+      avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
+    )
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'How old are you?', created_at: Date.parse('27.03.2016'))
+    ]
+    @new_question = Question.new
   end
 end
